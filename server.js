ce8d64549d224b0019bdc4053f4005a75973e7f1
Beardo Mattix <christopher.mattix@gmail.com>
2021-09-29 10:22:47 -0400

    updating connection file and adding comments

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 const inquirer = require("inquirer");
 // Imports the mysql package
 const mysql = require("mysql2");
-const connection = require("connection");
+const connection = require("./db/connection");
 // TODO: RENAME THIS FILE TO Index.js
+// FIXME:
 
 // const connection = mysql.createConnection({
 //   host: "localhost",
@@ -12,15 +13,17 @@ const connection = require("connection");
 //   password: "Finntroll1",
 //   database: "employees_db",
 // });
-// Move inquirer outside of server.js
+
 // connection.connect(function (err) {
 //   if (err) throw err;
 //   console.log("Now connected as ID" + connection.threadId);
 //   startPrompt();
 // });
-// This initializes the Inquirer package.
-// Make server file unique to server only actions.
-// Add detailed comments to show my understanding of the code.
+
+// TODO: Make server file unique to server only actions.
+//  TODO: Add detailed comments to show my understanding of the code.
+
+// Starts the inquireer packagage and allows users to answer prompts to interact with tables.
 function startPrompt() {
   inquirer
     .prompt([
@@ -71,7 +74,8 @@ function startPrompt() {
       }
     });
 }
-// Function that allows the user to view all employees in the DB
+/* Function that allows the user to view all employees in the DB. 
+Joins the information from the Employees, Managers, Roles, and Departments table into one table.*/
 function viewAllEmployees() {
   connection.query(
     "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department AS Department ON department.id = role.department_id LEFT JOIN employee e ON employee.manager_id = e.id;",
@@ -83,7 +87,7 @@ function viewAllEmployees() {
   );
 }
 
-// Function that allows the user to view all departments
+// Function that allows the user to view all departments by selecting all data from the managers table and displaying it in the terminal.
 function viewAllDepartments() {
   connection.query("SELECT * FROM department;", function (err, res) {
     if (err) throw err;
@@ -91,7 +95,7 @@ function viewAllDepartments() {
     startPrompt();
   });
 }
-// Function for showing the roles and departments
+// Function for showing the roles and departments. Selects the Rold ID, Title, Salary, and Department ID from the role table and joins the information together with the corresponding department.
 function viewAllRoles() {
   connection.query(
     "SELECT role.id, role.title, role.salary , role.department_id AS department FROM role JOIN department ON role.department_id = department.id;",
@@ -104,7 +108,10 @@ function viewAllRoles() {
     }
   );
 }
-// Function to add a role
+/* Function to add a role.
+Selects title and salary from the role table, and prompts the user to select a new role and salary.
+Inserts the new role (title) and salary into the role table.
+*/
 function addRole() {
   connection.query(
     "SELECT role.title AS title, role.salary AS salary FROM role;",
@@ -139,7 +146,10 @@ function addRole() {
     }
   );
 }
-// Function to add a department
+/* Function to add a department.
+Prompts user to name the new department.
+Inserts the new department into the Departments table in the name category.
+*/
 function addDepartment() {
   inquirer
     .prompt([
@@ -164,7 +174,7 @@ function addDepartment() {
     });
 }
 
-// Role queries
+// Role queries. Creates an empty array for Roles. Selects all from Role table and loops through to return the list of roles and add any new roles.
 const roleArray = [];
 function selectRole() {
   connection.query("SELECT * FROM role", function (err, res) {
@@ -175,7 +185,7 @@ function selectRole() {
   });
   return roleArray;
 }
-// Manager queries
+// Manager queries. Creates an empty array for Managers. Selects all from Managers table and loops through to return the list of roles and add any new Managers.
 const managersArray = [];
 function selectManager() {
   connection.query(
@@ -189,7 +199,10 @@ function selectManager() {
   );
   return managersArray;
 }
-// Function for adding an employee.
+/* Function for adding an employee.
+This fires the inquirer npm and prompts the user to enter the first name, last name, role, manager. 
+This is added to the DB and the selected role will have a salary attached based on the keys defined in the role table. 
+*/
 function addEmployee() {
   inquirer
     .prompt([
@@ -235,7 +248,11 @@ function addEmployee() {
       );
     });
 }
-// Function for updating the role of an employee
+/* Function for updating the role of an employee.
+Selects the last anem and title of the employee to be updated from the employee table.
+User selects the employee they want to update, zand then can update that employee's role.
+The updated employee role is inserted into the employee table based on employee's last name.
+*/
 function updateEmployee() {
   connection.query(
     "SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;",
